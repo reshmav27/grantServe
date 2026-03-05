fdda90750704c8d6d9b226473eb47da6144f53a5
reshmav27 <Reshma.V3@cognizant.com>
2026-03-05 14:41:34 +0530

    first commit

---
+++ src/main/java/com/cts/grantserve/GrantserveApplication.java
@@ -0,0 +1,13 @@
+package com.cts.grantserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrantserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrantserveApplication.class, args);
+	}
+
+}
