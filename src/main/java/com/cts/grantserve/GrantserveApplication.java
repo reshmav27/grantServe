db0744ff78d1273080dc0d2bef04acc03ec01408
SREEVARSHINI <sreevarshini2020@gmail.com>
2026-03-18 17:56:05 +0530

    merging with main

--- src/main/java/com/cts/grantserve/GrantserveApplication.java
+++ src/main/java/com/cts/grantserve/GrantserveApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GrantserveApplication {
+public class GrangtserveApplication {
 
 	public static void main(String[] args) {
 		// This is the ONLY line needed to start Spring and Hibernate
