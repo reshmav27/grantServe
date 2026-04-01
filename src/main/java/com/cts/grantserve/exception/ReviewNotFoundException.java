8f777e1e704e5ff08c3d5a4cdf57053e7b6748f6
anusriadirala23 <Anusri.Adirala@cognizant.com>
2026-04-01 09:21:28 +0530

    changes made and test files added for service classes

--- src/main/java/com/cts/grantserve/exception/ReviewNotFoundException.java
+++ src/main/java/com/cts/grantserve/exception/ReviewNotFoundException.java
@@ -1,7 +1,16 @@
 package com.cts.grantserve.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class ReviewNotFoundException extends RuntimeException {
-    public ReviewNotFoundException(String message) {
+
+    private HttpStatus httpStatus;
+    public ReviewNotFoundException(String message,HttpStatus httpStatus) {
         super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
     }
 }
